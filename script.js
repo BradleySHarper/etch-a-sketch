6a9de219592601b9cbb0e03a53bf90cb1a755e04
BradleySHarper <43481178+BradleySHarper@users.noreply.github.com>
2023-05-24 18:07:27 -0700

    Remove JQuery and add vanilla JS

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-const mdiv = document.querySelector('.container');
-const button = document.querySelector('button');
-
-for (let i=1; i < 257; i++ ){
-    const div = document.createElement('div');
-    mdiv.appendChild(div);
-}
+const container = document.querySelector('#container');
+const btn = document.querySelector('.newGrid');
 
 
-$('.container').children().on('mouseover', function(){
-    $(this).addClass('active');
-});
+// function that builds a grid in the "container"
+function createGrid(x) {
+    
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            const div = document.createElement('div');
+            div.className = "grid";
+            div.style.width = (700/x) + "px";
+            div.style.height = (700/x) + "px";
+            div.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
+            container.appendChild(div);
+            
+        };
+    };
+};
 
-button.addEventListener('click', () => {
+
+
+// function that clears the grid
+function clearGrid(parent){
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+};  
+
+// function that prompts the user to select the number of boxes in a new grid
+// the function then also creates that new grid
+function refreshGrid(){
+    var z = prompt("How many boxes per side?");
+    clearGrid(container);
+    createGrid(z);
+};
+
+btn.addEventListener('click', refreshGrid);
+createGrid(16);
+
+    
+    
+        
     
 
-})
+
+
+
+
+
+
 
 
 
