a47f0521badde8d42dd5972cbf823c1f1a600c63
BradleySHarper <43481178+BradleySHarper@users.noreply.github.com>
2023-05-26 10:10:59 -0700

    Add random RGB value for each mouseover event

--- script.js
+++ script.js
@@ -11,12 +11,32 @@ function createGrid(x) {
             div.className = "grid";
             div.style.width = (700/x) + "px";
             div.style.height = (700/x) + "px";
-            div.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
+            //div.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
+            div.addEventListener('mouseover', function(e){
+                e.target.style.backgroundColor = generateRandomColor();
+            });
             container.appendChild(div);
             
         };
     };
 };
+
+function pickColor(){
+    var colors = ['#faf000', '#00ff00', '#0000ff', '#ff3333', '#ff6600'];
+    var random_color = colors[(Math.floor(Math.random() * colors.length))];
+    console.log(random_color);
+    return random_color;
+        
+      
+}
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
 
 
 
